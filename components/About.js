b83398c2b28a511c80428e4e3abcdc7ce5c4914d
Suhas.m.malagihal <115938641+suhasm-18@users.noreply.github.com>
2023-08-03 17:19:23 +0530

    Create About.js

---
+++ components/About.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function About(props) {
+
+    // const [myStyle, setMyStyle] = useState({
+    //     color: 'black',
+    //     backgroundColor: 'white'
+    // }) 
