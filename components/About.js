58f0dfe1dbbb10eab1630ab5779e90bc3210d4bc
Suhas.m.malagihal <115938641+suhasm-18@users.noreply.github.com>
2023-08-03 17:20:36 +0530

    Update About.js

--- components/About.js
+++ components/About.js
@@ -6,3 +6,7 @@ export default function About(props) {
     //     color: 'black',
     //     backgroundColor: 'white'
     // }) 
+ let myStyle = {
+        color: props.mode ==='dark'?'white':'#042743',
+        backgroundColor: props.mode ==='dark'?'rgb(36 74 104)':'white', 
+    }
