84a1f4cd118f331489db783e76b9e951eb459e04
Suhas.m.malagihal <115938641+suhasm-18@users.noreply.github.com>
2023-08-03 17:21:22 +0530

    Update About.js

--- components/About.js
+++ components/About.js
@@ -10,3 +10,7 @@ export default function About(props) {
         color: props.mode ==='dark'?'white':'#042743',
         backgroundColor: props.mode ==='dark'?'rgb(36 74 104)':'white', 
     }
+ return (
+        <div className="container">
+            <h1 className="my-3" style={{color: props.mode ==='dark'?'white':'#042743'}}>About Us</h1>
+            <div className="accordion" id="accordionExample">
