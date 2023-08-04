c0ed2b20db83f6545f7dbf9b4b9b69e63b82a624
Suhas.m.malagihal <115938641+suhasm-18@users.noreply.github.com>
2023-08-04 11:21:51 +0530

    Update About.js

--- components/About.js
+++ components/About.js
@@ -14,3 +14,8 @@ export default function About(props) {
         <div className="container">
             <h1 className="my-3" style={{color: props.mode ==='dark'?'white':'#042743'}}>About Us</h1>
             <div className="accordion" id="accordionExample">
+<div className="accordion-item">
+                    <h2 className="accordion-header" id="headingOne">
+                    <button className="accordion-button" type="button" style={myStyle} data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                    <strong>Analyze Your text </strong>
+                    </button>
