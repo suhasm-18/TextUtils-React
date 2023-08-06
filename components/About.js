b8a13dc311a8260c334a870da9a191b9071feeab
Suhas.m.malagihal <115938641+suhasm-18@users.noreply.github.com>
2023-08-06 10:00:26 +0530

    Update About.js

--- components/About.js
+++ components/About.js
@@ -26,3 +26,15 @@ export default function About(props) {
  </div>
                     </div>
                 </div>
+<div className="accordion-item">
+                    <h2 className="accordion-header" id="headingTwo">
+                    <button className="accordion-button collapsed" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                    <strong>Free to use </strong>
+                    </button>
+                    </h2>
+                    <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                    <div className="accordion-body" style={myStyle}>
+                    TextUtils is a free character counter tool that provides instant character count & word count statistics for a given text. TextUtils reports the number of words and characters. Thus it is suitable for writing text with word/ character limit.
+                    </div>
+                    </div>
+                </div>
