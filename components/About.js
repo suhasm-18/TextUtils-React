6d75516b26590aedab0128d7470272030b0695f9
Suhas.m.malagihal <115938641+suhasm-18@users.noreply.github.com>
2023-08-06 10:00:40 +0530

    Update About.js

--- components/About.js
+++ components/About.js
@@ -38,3 +38,16 @@ export default function About(props) {
                     </div>
                     </div>
                 </div>
+<div className="accordion-item">
+                    <h2 className="accordion-header" id="headingThree">
+                    <button className="accordion-button collapsed" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                    <strong>Browser Compatible </strong>
+                    </button>
+                    </h2>
+                    <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                    <div className="accordion-body" style={myStyle}>
+                    This word counter software works in any web browsers such as Chrome, Firefox, Internet Explorer, Safari, Opera. It suits to count characters in facebook, blog, books, excel document, pdf document, essays, etc.
+
+                    </div>
+                    </div>
+                </div>
