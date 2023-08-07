013049f77ab045285558ec8b12ae372ee7109d1a
Suhas.m.malagihal <115938641+suhasm-18@users.noreply.github.com>
2023-08-07 22:50:33 +0530

    Create Alert.js

---
+++ components/Alert.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Alert(props) {
+    const capitalize = (word)=>{
+        const lower = word.toLowerCase();
+        return lower.charAt(0).toUpperCase() + lower.slice(1);
+    }
