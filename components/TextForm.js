862f1f009987e4bf38683cec6f3c8cd1c46eb336
Suhas.m.malagihal <115938641+suhasm-18@users.noreply.github.com>
2023-08-08 22:17:33 +0530

    Update TextForm.js

--- components/TextForm.js
+++ components/TextForm.js
@@ -7,3 +7,8 @@ export default function TextForm(props) {
         setText(newText)
         props.showAlert("Converted to uppercase!", "success");
     }
+ const handleLoClick = ()=>{ 
+        let newText = text.toLowerCase();
+        setText(newText)
+        props.showAlert("Converted to lowercase!", "success");
+    }
