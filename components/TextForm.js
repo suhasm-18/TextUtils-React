db96e5e6753b3e4f2fd1560eba5fd720e4e63aca
Suhas.m.malagihal <115938641+suhasm-18@users.noreply.github.com>
2023-08-08 22:16:18 +0530

    Update TextForm.js

--- components/TextForm.js
+++ components/TextForm.js
@@ -1,1 +1,9 @@
 import React, {useState} from 'react'
+
+
+export default function TextForm(props) {
+    const handleUpClick = ()=>{
+        let newText = text.toUpperCase();
+        setText(newText)
+        props.showAlert("Converted to uppercase!", "success");
+    }
