67b289b80d8c4e4bf3f3963c5ebd3c2cbc847ef2
Suhas.m.malagihal <115938641+suhasm-18@users.noreply.github.com>
2023-08-13 19:08:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -35,3 +35,26 @@ function App() {
       document.body.style.backgroundColor = 'white';
       showAlert("Light mode has been enabled", "success");
     }
+ return (
+    <>
+    <Router>
+    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} key={new Date()} />
+    <Alert alert={alert}/>
+    <div className="container my-3">
+    <Switch>
+    {/* /users --> Component 1
+        /users/home --> Component 2 */}
+          <Route exact path="/about">
+            <About mode={mode} />
+          </Route>
+          <Route exact path="/">
+            <TextForm showAlert={showAlert} heading="Try TextUtils - word counter, character counter, remove extra spaces" mode={mode}/>
+          </Route>
+    </Switch>
+    </div>
+    </Router>
+    </> 
+  );
+}
+
+export default App;
