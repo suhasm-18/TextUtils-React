75a98e048d5a1cbae9c0ce60ab5fb1830de29b82
Suhas.m.malagihal <115938641+suhasm-18@users.noreply.github.com>
2023-08-13 19:07:56 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import About from './components/About';
+import React, { useState } from 'react';
+import Alert from './components/Alert';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
